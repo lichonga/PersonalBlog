8b49f5dd811ff032dbfc910b5e5b1510e1748f48
lichonga <769648086@qq.com>
2019-10-07 00:04:08 +0800

    text1

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 function createContention(){
     var connection = mysql.createConnection({
-        host: "127.0.0.1",
+        host: "192.168.125.1",
         port: "3306",
         user: "root",
         password: "123456789",
         database: "my_blog"
+
     });
     return connection;
 };
